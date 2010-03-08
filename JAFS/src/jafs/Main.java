4e32f7cef3c1e260e1b6b3d9080623f29e6759ec
Julian.Sanio@gmx.de <Julian.Sanio@gmx.de@3aec694e-2ab1-11df-9816-a3eb4afb4c33>
2010-03-08 15:46:04 +0000

    test2

--- JAFS/src/jafs/Main.java
+++ JAFS/src/jafs/Main.java
@@ -18,6 +18,7 @@ public class Main {
         // TODO code application logic here
         System.out.println("test");
         System.out.println("test2");
+        System.out.println("test3");
     }
 
 }
