0a3e7cd37031a655ca1ba2ab2d52a1e37c35a8a7
Julian.Sanio@gmx.de <Julian.Sanio@gmx.de@3aec694e-2ab1-11df-9816-a3eb4afb4c33>
2010-03-13 21:15:08 +0000

    UserInterface Test 6, many fixes, Interfaces are painted correctly, setLocation is working.

--- trunk/Game/src/client/ui/MainMenu.java
+++ trunk/Game/src/client/ui/MainMenu.java
@@ -6,8 +6,6 @@
 
 package client.ui;
 
-import java.awt.event.ActionEvent;
-
 /**
  *
  * @author Julian Sanio
@@ -16,10 +14,8 @@ public class MainMenu extends UiWindow {
 
     /** Creates new form MainMenu */
     public MainMenu() {
-        super();
-        setSize(300, 300);
         initComponents();
-        setVisible(true);
+        initDecoration();
     }
 
     @SuppressWarnings("unchecked")
@@ -31,6 +27,9 @@ public class MainMenu extends UiWindow {
         jButton3 = new javax.swing.JButton();
         jButton4 = new javax.swing.JButton();
         jButton5 = new javax.swing.JButton();
+
+        setName("MainMenu"); // NOI18N
+        setPreferredSize(new java.awt.Dimension(150, 200));
 
         jButton1.setText("Server suchen");
 
@@ -46,19 +45,24 @@ public class MainMenu extends UiWindow {
         this.setLayout(layout);
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING))
             .addGroup(layout.createSequentialGroup()
-                .addGap(16, 16, 16)
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
-                    .addComponent(jButton4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jButton5, javax.swing.GroupLayout.Alignment.LEADING)))
+                .addContainerGap()
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                        .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGap(16, 16, 16)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jButton4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jButton5, javax.swing.GroupLayout.Alignment.LEADING))))
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
                 .addComponent(jButton1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton2)
@@ -67,7 +71,8 @@ public class MainMenu extends UiWindow {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton4)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton5))
+                .addComponent(jButton5)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
     }// </editor-fold>//GEN-END:initComponents
 
@@ -79,9 +84,5 @@ public class MainMenu extends UiWindow {
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
     // End of variables declaration//GEN-END:variables
-
-    public void actionPerformed(ActionEvent e) {
-        System.out.println("Launched ActionListener.");
-    }
 
 }
--- trunk/Game/src/client/ui/UiWindow.java
+++ trunk/Game/src/client/ui/UiWindow.java
@@ -4,14 +4,20 @@ import java.awt.Color;
 import java.awt.GradientPaint;
 import java.awt.Graphics2D;
 import java.awt.Point;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.awt.image.BufferStrategy;
 import javax.swing.JPanel;
 
 /**
  *
  * @author Julian Sanio
+ *
+ * setName(...) muss gesetzt werden (auch im GUI Designer unter den Eigenschaften möglich)!!!
+ *
  */
-public abstract class UiWindow extends JPanel {
+public abstract class UiWindow extends JPanel implements MouseListener, Runnable {
+
     public static final int borderWidth = 10;
 
     protected Point location;
@@ -19,7 +25,13 @@ public abstract class UiWindow extends JPanel {
     protected GradientPaint gradRight;
     protected GradientPaint gradTop;
     protected GradientPaint gradBottom;
+
     protected BufferStrategy buffer;
+
+    protected boolean isMoveable;
+    protected boolean isMousePressed;
+    protected MouseEvent mouse = null;
+    protected Thread t;
 
     public UiWindow() {
         super();
@@ -27,17 +39,53 @@ public abstract class UiWindow extends JPanel {
         this.setDoubleBuffered(false);
         this.setIgnoreRepaint(true);
 
+        initDecoration();
+        isMoveable = true;
+        isMousePressed = false;
+        addMouseListener(this);
+        t = new Thread(this);
+        t.start();
+    }
+
+    public void initDecoration(){
         Color from = this.getBackground();
         Color to = new Color(from.getRed(), from.getGreen(), from.getBlue(), 0);
 
         gradLeft = new GradientPaint(0, 0, from, borderWidth, 0, to, true);
-        gradRight = new GradientPaint(0, 0, from, borderWidth, 0, to, true);
-        gradTop = new GradientPaint(0, borderWidth, from, borderWidth, 0, to, true);
-        gradBottom = new GradientPaint(0, 0, from, 0, borderWidth, to, true);
+        gradRight = new GradientPaint(0, 0, to, borderWidth, 0, from, true);
+        gradTop = new GradientPaint(0, 0, from, 0, borderWidth, to, true);
+        gradBottom = new GradientPaint(0, 0, from, 0, borderWidth, to, true);  
     }
+
+    public void run(){
+        while(t.isAlive()){
+            if (mouse != null && isMousePressed)
+                UiManager.changeLocation(getName(), mouse.getXOnScreen(), mouse.getYOnScreen()-50);
+//            try {
+//                Thread.sleep(500);
+//            } catch (InterruptedException ex) {  }
+        }
+    }
+
+    public void mouseClicked(MouseEvent e) {  }
+
+    public void mousePressed(MouseEvent e) {
+        isMousePressed = true;
+    }
+
+    public void mouseReleased(MouseEvent e) {
+        isMousePressed = false;
+    }
+
+    public void mouseEntered(MouseEvent e) {
+        mouse = e;
+    }
+
+    public void mouseExited(MouseEvent e) {  }
 
     public void render(Graphics2D g) {
         if(isVisible() && g != null) {
+            setSize(getPreferredSize().width, getPreferredSize().height); // !!! bessere Implementierung?
             location = getLocation();
             g.translate(location.x, location.y);
             paint(g);
@@ -47,13 +95,25 @@ public abstract class UiWindow extends JPanel {
     }
 
     public void renderDecoration(Graphics2D g) {
-        g.setPaint(gradLeft);
-        g.fillRect(getWidth(), 0, borderWidth, getHeight());
-        g.setPaint(gradLeft);
-        g.fillRect(-borderWidth, 0, borderWidth, getHeight());
-        g.setPaint(gradBottom);
+        // Right
+//        g.setPaint(gradRight);
+//        g.fillRect(getWidth(), 0, borderWidth, getHeight());
+        // Left
+//        g.setPaint(gradLeft);
+//        g.fillRect(-borderWidth, 0, borderWidth, getHeight());
+        // Top
+        g.setPaint(gradTop);
         g.fillRect(0, -borderWidth, getWidth(), borderWidth);
+        // Bottom
         g.setPaint(gradBottom);
         g.fillRect(0, getHeight(), getWidth(), borderWidth);
+    }
+
+    public boolean isMoveable(){
+        return isMoveable;
+    }
+
+    public void setMoveable(boolean b){
+        isMoveable = b;
     }
 }
