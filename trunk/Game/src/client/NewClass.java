ac3eb23b8e7a1ba91ab87da6e238559887410b67
ft2011 <ft2011@3aec694e-2ab1-11df-9816-a3eb4afb4c33>
2010-03-23 18:12:04 +0000

    Timer-Task exception fixxed. (thx alex!)

---
+++ trunk/Game/src/client/NewClass.java
@@ -0,0 +1,14 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ *** http://code.google.com/p/jafs/ ***
+ *** (c) J.A.F.S 20101 (c) ***
+package client;
+
+/**
+ *
+ * @author adm1n
+ */
+public class NewClass {
+
+}
